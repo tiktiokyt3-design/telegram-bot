1cdabc5bd37f0e93ef092279db4e94e5567b7692
tiktiokyt3-design <tiktiokyt3@gmail.com>
2025-09-19 23:03:59 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,27 @@
+from telegram.ext import Updater, CommandHandler
+
+# Твій токен від BotFather
+TOKEN = "7377335524:AAHM1DfWje4UoiR0e6BqZfxlz1oXHBJs5e4"
+
+# Функція відповіді на /start
+def start(update, context):
+    update.message.reply_text("Привіт 👋 Я твій перший бот!")
+
+# Функція відповіді на /help
+def help_command(update, context):
+    update.message.reply_text("Команди:\n/start - почати\n/help - допомога")
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    # Команди
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help_command))
+
+    # Запуск
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
